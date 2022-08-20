17682fd329d4ec75cb2e17e2b36cbdfaa9e83079
Azam-math <111635060+Azam-math@users.noreply.github.com>
2022-08-20 14:12:54 +0430

    Add files via upload

---
+++ project 10.py
@@ -0,0 +1,7 @@
+d1= float(input ("Enter d1="))
+d2= float(input ("Enter d2="))
+d3= float(input ("Enter d3="))
+if(d1+d2>d3 and d1+d3>d2 and d2+d3>d1):
+    print("It is a triangle")
+else:
+    print("It is not a triangle")
